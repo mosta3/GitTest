96d3d9daec6c2f3752135e7df5ddae9fab62225d
mosta <mosta@dev1>
2017-11-02 17:07:56 +0200

    another Template in Git one enter
    2 enter

---
+++ Client/src/testGitApp/Class1.java
@@ -0,0 +1,11 @@
+package testGitApp;
+
+public class Class1 {
+    public Class1() {
+        super();
+    }
+
+    public static void main(String[] args) {
+        Class1 class1 = new Class1();
+    }
+}
