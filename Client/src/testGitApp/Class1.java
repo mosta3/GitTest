9400f6828ed51527c0780508e00c6961452097fa
mosta <mosta@dev1>
2017-11-05 11:00:22 +0200

    this is my first comment Template
    in 
    Git

--- Client/src/testGitApp/Class1.java
+++ Client/src/testGitApp/Class1.java
@@ -8,4 +8,9 @@ public class Class1 {
     public static void main(String[] args) {
         Class1 class1 = new Class1();
     }
+    
+    public String sayHello(String name)
+    {
+        return "Hello "+name;
+    }
 }
